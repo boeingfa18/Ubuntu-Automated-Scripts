208d0210f569654f52ea03ef53add8c4a18014e4
boeingfa18 <boeinghornet2010@gmail.com>
2025-12-06 20:21:42 +0000

    Update install_waydroid.sh
    Made it so that Waydroid doesent actually start as soon as installation finishes but rather tells the user what commands to run in order to start Waydroid after installation finishes as to give them a choice.

--- install_waydroid.sh
+++ install_waydroid.sh
@@ -6,37 +6,34 @@ echo "   Waydroid Installer for Ubuntu / Ubuntu Based Distro"
 echo "----------------------------------------------------------"
 
 # 1. Update system
-echo "[1/7] Updating system..."
+echo "[1/6] Updating system..."
 sudo apt update && sudo apt upgrade -y
 
 # 2. Install dependencies
-echo "[2/7] Installing required packages..."
+echo "[2/6] Installing required packages..."
 sudo apt install -y curl ca-certificates lxc
 
 # 3. Add Waydroid repository
-echo "[3/7] Adding Waydroid repository..."
+echo "[3/6] Adding Waydroid repository..."
 curl -s https://repo.waydro.id | sudo bash
 
 # 4. Install Waydroid
-echo "[4/7] Installing Waydroid..."
+echo "[4/6] Installing Waydroid..."
 sudo apt install -y waydroid
 
 # 5. Initialize Waydroid
-echo "[5/7] Initializing Waydroid container..."
+echo "[5/6] Initializing Waydroid container..."
 sudo waydroid init
 
 # If you want GAPPS automatically, replace above line with:
 # sudo waydroid init -s GAPPS
 
 # 6. Enable container service
-echo "[6/7] Enabling Waydroid container service..."
+echo "[6/6] Enabling Waydroid container service..."
 sudo systemctl enable --now waydroid-container
-
-# 7. Start session
-echo "[7/7] Starting Waydroid session..."
-waydroid session start &
 
 echo "--------------------------------------------------"
 echo "  ✔ Waydroid installation finished!"
+echo "  To launch start a Waydrioid session: waydroid session start"
 echo "  To launch full Android UI: waydroid show-full-ui"
 echo "--------------------------------------------------"
